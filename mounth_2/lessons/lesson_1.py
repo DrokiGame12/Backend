c677d1be7aac96e0c8a83d3d8801cd9e9121e71f
DrokiGame12 <DrokiGame.12@gmail.com>
2024-09-01 14:04:15 +0600

    GIT 2.1

--- mounth_2/lessons/lesson_1.py
+++ mounth_2/lessons/lesson_1.py
@@ -19,8 +19,7 @@
 # 		dunction method
 # 	...
 
-class Car:
-	# __init__ - constructor
+class Transport:
 	def __init__(self, the_year, the_model, the_color, penalties=0):
 		# fields / attributs
 		self.year = the_year
@@ -28,6 +27,24 @@ class Car:
 		self.color = the_color
 		self.penalties = penalties
 
+	def change_color(self, new_color):
+		self.color = new_color
+
+class Plane(Transport):
+	counter = 0
+	def __init__(self, the_year, the_model, the_color):
+		super.__init__(the_year, the_model, the_color)
+		Plane.counter += 1
+
+
+class Car(Transport):
+	counter = 0
+	# __init__ - constructor
+	def __init__(self, the_year, the_model, the_color, penalties=0):
+		# super class
+		super().__init__(the_year, the_model, the_color)	# constructor matching
+		self.penalties = penalties
+		Car.counter += 1
 	# method
 	def drive(self):
 		print(f'Car {self.model} is driving...')
@@ -36,6 +53,10 @@ class Car:
 		while nunmber_of_times > 0:
 			print(f'Car {self.model} signaling {sound}')
 			nunmber_of_times -= 1
+
+class Truck(Car):
+	def __init__(self, the_year, the_model, the_color, penalties=0):
+		super().__init__(the_year, the_model, the_color, penalties)
 
 bmw_car = Car(2020, "BMW X6", 'red')
 print(bmw_car.penalties)
@@ -49,5 +70,4 @@ honda_car.signal(2, 'Krya')
 
 print('STOP')
 print('END')
-
 
